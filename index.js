ea134b14b2f54e6cfc438841a9b28a7bdb0f4771
Felix Martinez <felixaviermartinez@gmail.com>
2019-07-15 13:28:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// add solution here
+
+
+function theBeatlesPlay(musicians, instruments) {
+  const empty = [];
+  for (let i = 0; i <= 3; i++) {
+    empty.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return empty;
+}
+
+function johnLennonFacts(array) {
+  const factsArray = [];
+  let i = 0;
+  while (i < array.length) {
+    factsArray.push(array[i] + '!!!');
+    i++;
+  }
+  return factsArray;
+}
+
+function iLoveTheBeatles(num) {
+  const aArray = [];
+  do {
+    aArray.push('I love the beatles!');
+  } while (num < 15); {
+    num++;
+    aArray.push('I love the beatles!');
+  }
+  return aArray;
+}
+
